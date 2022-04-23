f749788cd3fcdd1801b78d7acacce7f412eb4430
nakulsharma15 <nakuls309@gmail.com>
2022-04-23 23:45:09 +0530

    Feat: added basic routing

---
+++ src/Pages/Explore.jsx
@@ -0,0 +1,8 @@
+export default function Explore() {
+
+    return (
+        <div>
+            <h1>This is Explore.</h1>
+        </div>)
+
+}
---
+++ src/Pages/index.js
@@ -0,0 +1,21 @@
+import Home from "./Home";
+import Explore from "./Explore";
+import WatchLater from "./WatchLater";
+import Login from "./Login";
+import Signup from "./Signup";
+import Liked from "./Liked";
+import MockMan from "./MockMan";
+import Playlist from "./Playlist";
+import History from "./History";
+
+export {
+    Home,
+    Explore,
+    WatchLater,
+    Login,
+    Signup,
+    Liked,
+    MockMan,
+    Playlist,
+    History
+}
