29076803f5a3b4056114d93223e2169050dffc5c
nakulsharma15 <nakuls309@gmail.com>
2022-05-02 00:48:45 +0530

    Update videos.js

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,120 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "8CCtqtva6ok",
+    title: "POCO X4 Pro Review - $250 iPhone Destroyer?",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163019/PixelPlay%20Video%20Library/Smartphone%20Video%20Thumbnails/hq720_dtfwm4.jpg",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Unboxing and Review of the POCO X4 Pro, the best budget phone in 2022, including Camera test, battery life, and price, but then also the sneaky ways that POCO manages to make their phones so cheap!",
+    creator: "Mrwhosetheboss",
+    category: "Smartphones"
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "QpbGctuHoMY",
+    title: "iPhone SE (2022) Review: Old Dog. New Trick.",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163058/PixelPlay%20Video%20Library/Smartphone%20Video%20Thumbnails/hqdefault_hlhwpp.jpg",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "iPhone SE = Special Edition, but really the formula is very simple...",
+    creator: "Marques Brownlee",
+    category:"Smartphones"
+  },
+  {
+    _id: "vXIAB_1FEC0",
+    title: "Galaxy S22 Ultra Review: Separating from the Pack!",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163101/PixelPlay%20Video%20Library/Smartphone%20Video%20Thumbnails/hq720_fjebad.jpg",
+    description:
+      "Samsung's Galaxy S22 Ultra separates itself from the rest of the lineup with more than just specs.",
+    creator: "Marques Brownlee",
+    category:"Smartphones"
+  },
+  {
+    _id: "5EuUXxyOoBY",
+    title: "India's Fastest Smartphone | iQOO Z6 Pro 5G Unboxing🔥🔥🔥",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163160/PixelPlay%20Video%20Library/Smartphone%20Video%20Thumbnails/hq720_ju6oid.jpg",
+    description:
+      "Namaskar Dosto, maine is video mein aapse #iQOOZ6 Pro5G #FullyLoaded Smartphone ke baare mein baat ki hai aur share ki hai iQOO Z6 Pro 5G Smartphone ki unboxing aur ek first look.",
+    creator: "Technical Guruji",
+    category:"Smartphones"
+  },
+  {
+    _id: "UIbBVlD80TA",
+    title: "Moto G52 Unboxing *Budget Phone Flagship Display*!",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163212/PixelPlay%20Video%20Library/Smartphone%20Video%20Thumbnails/hqdefault_krbd9o.jpg",
+    description:
+      "Naya Moto dekh liya jaye ! This Video is made in #Collab with Motorola India",
+    creator: "Tech Burner",
+    category:"Smartphones"
+  },
+  {
+    _id: "NSVUw3KF5PE",
+    title: "MSI Stealth GS77 Review",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163328/PixelPlay%20Video%20Library/Laptop%20Video%20Thumbnails/hq720_qfa4kf.jpg",
+    description:
+      "My review of the MSI Stealth GS77 and MSI Creator Z16P. New Laptops for 2022 with WAY better performance.",
+    creator: "Dave2D",
+    category:"Laptops"
+  },
+  {
+    _id: "s8dFxCLzN8M",
+    title: "ASUS BR1100C: Best Windows Laptop Under 25K?",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163371/PixelPlay%20Video%20Library/Laptop%20Video%20Thumbnails/hq720_qnwrgg.jpg",
+    description:
+      "ASUS has launched its new BR1100 series of budget laptops in India and in this video, we focus on the ASUS BR1100C and answer the question, is it the best Windows 11 laptop under 25K?",
+    creator: "Beebom",
+    category:"Laptops"
+  },
+  {
+    _id: "As7gTv6-BTA",
+    title: "Samsung Galaxy Book2 360: The Samsung Ecosystem!",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163415/PixelPlay%20Video%20Library/Laptop%20Video%20Thumbnails/hqdefault_lxkb26.jpg",
+    description:
+      "Samsung has launched its new Galaxy Book laptops in India. In this video, we take a look at the Samsung Galaxy Book2 360, which has a Super AMOLED display, 12th gen Intel CPU, sleek and lightweight design and some really cool ecosystem features.",
+    creator: "Beebom",
+    category:"Laptops"
+  },
+  {
+    _id: "rr2XfL_df3o",
+    title: "M1 Max MacBook Pro Review: Truly Next Level!",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163505/PixelPlay%20Video%20Library/Laptop%20Video%20Thumbnails/hq720_sxuxwe.jpg",
+    description:
+      "The 14 and 16 inch MacBook Pros are incredible. I can finally retire the travel iMac.",
+    creator: "Marques Brownlee",
+    category:"Laptops"
+  },
+  {
+    _id: "M6qoYGD3fB0",
+    title: "Why Netflix Is DYING? The Huge Stock Crash Explained🔥🔥🔥",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163578/PixelPlay%20Video%20Library/Tech%20Explained%20Video%20Thumbnails/hqdefault_zwm6xc.jpg",
+    description:
+      "Namaskaar Dosto, is video mein maine aapse baat ki hai Netflix ke huge decline ke baare mein aur yaha pe ek hi din mein 35% stock ka decline hua hai.",
+    creator: "Technical Guruji",
+    category:"Tech Explained"
+  },
+  {
+    _id: "QdkRuu4jvsk",
+    title: "The Samsung Smartphone Scandal: Explained",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163626/PixelPlay%20Video%20Library/Tech%20Explained%20Video%20Thumbnails/hq720_bnm5vy.jpg",
+    description:
+      "This is what you need to know about the SamsunG GOS Scandal and what it means for your next phone! ",
+    creator: "Mrwhosetheboss",
+    category:"Tech Explained"
+  },
+  {
+    _id: "UpqaQR4ikig",
+    title: "Does Fast Charging ACTUALLY Ruin Your Battery?",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163719/PixelPlay%20Video%20Library/Tech%20Explained%20Video%20Thumbnails/hqdefault_hf3qcf.jpg",
+    description:
+      "60 watts? 120 watts? 240 watts? Here's what REALLY ruins batteries, explained.",
+    creator: "Marques Brownlee",
+    category:"Tech Explained"
+  },
+  {
+    _id: "O3BvQD407Pk",
+    title: "Smartphone OTA Updates! The Pathetic New Reality - RAW Thoughts",
+    thumbnail: "https://res.cloudinary.com/nakulsharma15/image/upload/v1651163838/PixelPlay%20Video%20Library/Tech%20Explained%20Video%20Thumbnails/hqdefault_i6f8cm.jpg",
+    description:
+      "In this video my raw thoughts about the updates that we get via OTA on our Android smartphones have noticed lately many of the updates have started crippling the devices and for older devices many users started having hardware issues after applying the OTA updates on their android smartphones.",
+    creator: "Geekyranjit",
+    category:"Tech Explained"
   },
 ];
