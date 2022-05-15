b28049393b5ebeaf9f87038b1c7682768886b84f
nakulsharma15 <nakuls309@gmail.com>
2022-05-15 21:50:57 +0530

    feat: added react-youtube

--- src/Pages/VideoPage.jsx
+++ src/Pages/VideoPage.jsx
@@ -1,8 +1,18 @@
 import { Header,  SideNav} from "../Components";
 import "./Styles.css";
-
+import YouTube from 'react-youtube';
 
 export default function VideoPage() {
+
+    const videoId = "C1YfOTAD42M";
+    const opts = {
+        height: '390',
+        width: '780',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters for more info
+          autoplay: 1,
+        },
+      };
 
     return (
         <div>
@@ -14,7 +24,9 @@ export default function VideoPage() {
 
                 <div className="main-content">
                    <div className="video-section">
-                       <div className="video-player"></div>
+                       <div className="video-player">
+                       <YouTube videoId={videoId} opts={opts}/>
+                       </div>
                        <div className="video-actions"></div>
                    </div>
                 </div>
