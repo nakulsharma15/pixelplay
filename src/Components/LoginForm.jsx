8ee715b61041a9804af8e67041ed58f6ed072b51
nakulsharma15 <nakuls309@gmail.com>
2022-05-20 19:20:39 +0530

    feat: created login form

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,9 +1,34 @@
+import "./Styles/LoginForm.css";
+
 export default function LoginForm() {
 
 
     return (
-        <div>
-            <h1>this is login Component</h1>
+        <div className="form-div-parent">
+            <form>
+
+               <div className="form-div email-div">
+                   <label for="email">Email</label>
+                   <input className="email" type="email" placeholder="Enter Email"/>
+               </div>
+
+               <div className="form-div">
+                   <label for="password">Password</label>
+                   <input className="password" type="password" placeholder="Enter Password"/>
+               </div>
+
+               <button className="signup-btn">Login using guest credentials</button>
+
+
+               <div className="form-action-div">
+
+                   <button className="signup-btn">Create account</button>   
+
+                   <button className="submit-btn primary-btn">Login</button>
+
+               </div>
+
+            </form>
         </div>
     )
 
