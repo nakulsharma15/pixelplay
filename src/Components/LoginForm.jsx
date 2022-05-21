00604d8b82fe2406443c3c4f2f30f5b8305a6472
nakulsharma15 <nakuls309@gmail.com>
2022-05-21 19:08:01 +0530

    feat: added formik and Yup

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,21 +1,44 @@
 import "./Styles/LoginForm.css";
 import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function LoginForm() {
 
+    const formik = useFormik({
+        initialValues:{
+            email:"",
+            password:"",
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().required("Username is empty"),
+            password: Yup.string().required("Password is empty")
+        }),
+        onSubmit: (values , actions) => {
+            alert(JSON.stringify(values,null,2));
+            actions.resetForm();
+        }
+    });
+
+    let emailError = ((formik.errors.email !== undefined) && formik.touched.email) ? true : false; 
+
+    let passwordError = ((formik.errors.password !== undefined) && formik.touched.password) ? true : false; 
+    
 
     return (
         <div className="form-div-parent">
-            <form>
+            <form onSubmit={formik.handleSubmit}>
 
-                <div className="form-div email-div">
+                <div className={ emailError==true ? "form-div email-div form-error" :"form-div email-div"}>
                     <label htmlFor="email">Email</label>
-                    <input className="email" type="email" placeholder="Enter Email" />
+                    <input className="email" type="email" onChange={formik.handleChange} value={formik.values.email} placeholder="Enter Email" name="email"  />
+                    <p className="error-message">{formik.errors.email}</p>
                 </div>
 
-                <div className="form-div">
+                <div className={passwordError==true ? "form-div email-div form-error" :"form-div email-div"}>
                     <label htmlFor="password">Password</label>
-                    <input className="password" type="password" placeholder="Enter Password" />
+                    <input className="password" type="password" placeholder="Enter Password" onChange={formik.handleChange} value={formik.values.password} name="password" />
+                    <p className="error-message">{formik.errors.password}</p>
                 </div>
 
                 <p className="action-txt">Login using guest credentials</p>
@@ -25,7 +48,7 @@ export default function LoginForm() {
 
                     <Link to="/signup" className="action-txt">Create account</Link>
 
-                    <button className="submit-btn primary-btn">Login</button>
+                    <button className="submit-btn primary-btn" type="submit">Login</button>
 
                 </div>
 
